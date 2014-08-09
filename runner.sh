0ed34067d37e75a5016c6962ad93c754fbca9eda
Marcin Kolny <marcin.kolny@gmail.com>
2014-08-09 09:36:47 +0200

    ugly bug fixed

--- runner.sh
+++ runner.sh
@@ -1,1 +1,1 @@
-./build/src/raspistreamer --serial-speed 57600 --mpl-port 5009 --mpl-ip 192.168.1.30 --ip 192.168.1.30 --port 5004 -w 768 -h 576 -fps 30 -b 10000000
+./build/src/raspistreamer --max-mavlink-cnt 20 --serial-speed 57600 --mpl-port 5009 --mpl-ip 192.168.1.30 --ip 192.168.1.30 --port 5004 -w 768 -h 576 -fps 30 -b 10000000
